07346592da7f70882e1e9999e95a5b178cf438da
Arvind Kumar Meena <142485855+Aarv36@users.noreply.github.com>
2024-07-19 15:15:57 +0530

    Update settings.py

--- new_analysis/settings.py
+++ new_analysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3hhj-us0ujdyp1hxurmr86gr=bx2aysvy^2lk1%byv^%ke%=5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
