d7940418c134a56848a1866f6fad550723925a9a
Arvind Kumar Meena <142485855+Aarv36@users.noreply.github.com>
2024-07-24 19:42:47 +0530

    Update urls.py

--- analysis/urls.py
+++ analysis/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.upload_file, name='upload_file'),
     path('analyze/<int:file_id>/', views.analyze_file, name='analyze_file'),
     path('download/<int:file_id>/<str:format>/', views.download_highlighted_images, name='download_highlighted_images'),
+    path('download_word_file/<int:file_id>/', views.download_word_file, name='download_word_file'),
 ]
 
 if settings.DEBUG:
